ce819b4a7facdf83d4fb6190f534906bfdf31c18
Mohamed Khattab <mohamedkhattab@protonmail.com>
2017-09-10 15:30:19 +0200

    initial commit

--- resources/public/js/cljs-dev/.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 1.9.908 {}
-
-//# sourceMappingURL=.js.map?rel=1504962471111
--- resources/public/js/cljs-dev/figwheel/connect/build_dev.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 1.9.908 {}
-goog.provide('figwheel.connect.build_dev');
-goog.require('cljs.core');
-goog.require('figwheel.client');
-goog.require('figwheel.client.utils');
-figwheel.client.start.call(null,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"build-id","build-id",1642831089),"dev",new cljs.core.Keyword(null,"websocket-url","websocket-url",-490444938),"ws://localhost:3449/figwheel-ws"], null));
-
-//# sourceMappingURL=build_dev.js.map?rel=1504991300496
